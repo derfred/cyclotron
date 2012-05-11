f04394a7f60911954b54279584902f86faa3f54d
Frederik Fix <ich@derfred.com>
2012-05-11 09:22:24 +0200

    add warning if processing command was killed

--- wrap_compile.sh
+++ wrap_compile.sh
@@ -8,4 +8,13 @@ case $1 in
   sage)
     /usr/nld/sage-4.8-fat-x86_64-Linux/sage -python ${@:2}
     ;;
+  *)
+    echo "invalid command" 1>&2
+    exit
+    ;;
 esac
+
+xs=$?
+if [ $xs -gt 127 ]; then
+  echo "failed on $@" 1>&2
+fi
