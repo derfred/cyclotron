99af9d307c6b005ff35a7a540a978fe66b5e3b6e
Frederik Fix <ich@derfred.com>
2012-05-20 14:34:03 +0200

    properly name the clique filtering jobs

--- queue_filter.sh
+++ queue_filter.sh
@@ -9,10 +9,12 @@ MAXLEN=$3
 JOBSUFFIX=$4
 
 QSUB="qsub -cwd -V -q frigg.q,skadi.q -b y -o /dev/null -e $BASE/$PROBLEM/stderrs/"
+
+# step six, filter incomplete cliques
 for d in `ls $BASE/$PROBLEM/unique_cliques`
 do
   for i in {0..399}
   do
-    $QSUB -hold_jid dedup_cliques$JOBSUFFIX bash wrap_compile.sh sage filter_cliques.sage $PROBLEM.problem $BASE/$PROBLEM $MAXLEN ${d/.pickle/} $i
+    $QSUB -N filter_cliques$JOBSUFFIX bash wrap_compile.sh sage filter_cliques.sage $PROBLEM.problem $BASE/$PROBLEM $MAXLEN ${d/.pickle/} $i
   done
 done
