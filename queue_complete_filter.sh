0128d3fcdcc087f9eeb7980153921e0dfb7171f2
Frederik Fix <ich@derfred.com>
2012-05-21 09:40:59 +0200

    rename this

--- queue_complete_filter.sh
+++ queue_complete_filter.sh
@@ -15,6 +15,6 @@ for d in `ls $BASE/$PROBLEM/unique_cliques`
 do
   for i in {0..399}
   do
-    $QSUB -N filter_cliques$JOBSUFFIX bash wrap_compile.sh sage filter_cliques.sage $PROBLEM.problem $BASE/$PROBLEM $MAXLEN ${d/.pickle/} $i
+    $QSUB -N filter_cliques$JOBSUFFIX bash wrap_compile.sh sage filter_complete_cliques.sage $PROBLEM.problem $BASE/$PROBLEM $MAXLEN ${d/.pickle/} $i
   done
 done
