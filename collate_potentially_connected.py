cc64d2f0da6ef5719024fdcf8b4b0b9b5d790c95
Frederik Fix <ich@derfred.com>
2012-06-16 17:38:44 +0200

    collate potentially connected cliques

---
+++ collate_potentially_connected.py
@@ -0,0 +1,30 @@
+#
+# Fourth step of the decoding finding pipeline. This one is serial. usage:
+#  python collate_potentially_connected.py <basedir>
+#
+
+import pickle, os, time, operator, sys, fnmatch, collections
+
+basedir = sys.argv[1]
+
+def index(comb):
+  return comb[1]+comb[0]*len(cycles)
+
+files = collections.defaultdict(set)
+for path, dirs, files in os.walk(os.path.abspath("%s/collated_potentially_connected_cliques")):
+  for filename in fnmatch.filter(files, "*.pickle"):
+    files[int(filename.replace(".pickle", ""))].add(os.path.join(path, filename))
+
+result = collections.defaultdict(set)
+for length, fnames in files.iteritems():
+  for fname in fnames:
+    with open(fname) as f:
+      cliques = pickle.load(f)
+    for clique in cliques:
+      result[length].add(clique)
+
+print len(result)
+
+for k, v in result.iteritems():
+  with open("%s/unique_potentially_connected_cliques/%d.pickle"%(basedir, k), "w") as f:
+    pickle.dump(list(v), f)
