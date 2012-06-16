5b0ce9a4a0a182a7bc021738587cf84d94a8e45a
Frederik Fix <ich@derfred.com>
2012-06-16 11:24:03 +0200

    add deduplication to compile pipeline

--- queue_connected_filter.sh
+++ queue_connected_filter.sh
@@ -18,5 +18,7 @@ for d in `ls $BASE/$PROBLEM/unique_cliques`
 do
   mkdir -p $BASE/$PROBLEM/connected_cliques/${d/.pickle/}
   mkdir -p $BASE/$PROBLEM/potentially_connected_cliques/${d/.pickle/}
-  $QSUB -t 1-400 -N filter_connected_cliques$JOBSUFFIX bash wrap_compile.sh filter_connected_cliques.py $PROBLEM.problem $BASE/$PROBLEM $MAXLEN ${d/.pickle/}
+  mkdir -p $BASE/$PROBLEM/unique_potentially_connected_cliques/${d/.pickle/}
+  $QSUB -t 1-400 -N filter_${d/.pickle/}_connected_cliques$JOBSUFFIX bash wrap_compile.sh filter_connected_cliques.py $PROBLEM.problem $BASE/$PROBLEM $MAXLEN ${d/.pickle/}
+  $QSUB -hold_jid filter_${d/.pickle/}_connected_cliques$JOBSUFFIX bash wrap_compile.sh deduplicate_cliques.py $BASE/$PROBLEM potentially_connected_cliques/${d/.pickle/} unique_potentially_connected_cliques/${d/.pickle/}
 done
